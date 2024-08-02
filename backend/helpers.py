c68b23389db9f7a45830b01e96308bdcd0d5ee18
tomd09 <tomdowling009@gmail.com>
2024-08-02 16:12:39 +0800

    add new item now working fully with a change to the db to reflect new image title link column

--- backend/helpers.py
+++ backend/helpers.py
@@ -23,6 +23,26 @@ def retrieveTable(table):
         df = pd.read_sql(text(f'SELECT * FROM {table};'), conn)
     return df
 
+def addNewItem(url, name, type):
+    try:
+        session = requests.Session()
+        session.headers.update({
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+        })
+        response = session.get(url)
+        htmlContent = response.text
+        soup = BeautifulSoup(htmlContent, 'html.parser')
+        priceDiv = soup.find('div', class_='a-section a-spacing-none aok-align-center aok-relative')
+        price = None
+        if priceDiv:
+            price = float(priceDiv.text.strip().split(' ')[-1][1:])
+        df = pd.DataFrame([[name, type, url, price, datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')]], columns=['Name', 'Type', 'Link', 'Price', 'Time'])
+        engine = initialiseConnection()
+        df.to_sql(name='amazonprices', con=engine, index=False, if_exists='append')
+    except:
+        print('Invalid Amazon URL Supplied')
+
+
 def creatingSoup(url):
     #beautifulsoup to scrape amazon url with session to stop blockers
     session = requests.Session()
@@ -65,8 +85,8 @@ def addNewItem(url, name, type):
     existingDf = retrieveTable('amazonprices')
     uniqueURLs = list(existingDf['Link'].unique())
     new = False
-    if url not in uniqueURLs:
-        new = True
+    #if url not in uniqueURLs:
+        #new = True
     try:
         soup = creatingSoup(url)
         imageLinkTitle = creatingImageLinkTitle(soup)
